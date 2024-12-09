9f89bee395d1e6ba40030d034e512507b32a1b75
Steve <lysoll8444@gmail.com>
2024-12-09 15:41:25 -0600

    deleteMethod

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ class Item(BaseModel):
     name: str
     price: float
     brand: Optional[str]
+    
+class UpdateItem(BaseModel):
+    name: Optional[str] = None
+    price: Optional[float] = None
+    brand: Optional[str] = None
 
 @app.get("/")
 def root():
@@ -46,10 +51,36 @@ def get_item(item_name: str = None): #Code is saying if there is no passed value
 @app.post("/create-item/{item_id}")
 def create_item(item_id: int, item: Item):
 	if item_id in inventory:
-		return "Error"
+		return "Error: Already Exists"
 	inventory[item_id] = item
 	return inventory[item_id]
+
+#Updates an existing item
+@app.put("/update-item/{item_id}")
+def update_item(item_id: int, item:UpdateItem):
+    if item_id not in inventory: #If item doesn't exist, can't update
+        return {"error": "Item does not exist"}
+    
+    #If so, change the item at said id with new contents
+    #By default, values are None from the UpdateItem class, if it isn't None, update the contents
+    #None is needed because what if you dont want to  update contents
+    if item.name != None:
+        inventory[item_id].name = item.name 
+    if item.price != None:
+        inventory[item_id].price = item.price
+    if item.brand != None:
+        inventory[item_id].brand = item.brand
+    return inventory[item_id] #Return the new item for output
  
+#Deletes item with associated id
+@app.delete("/delete-item/")
+def delete_item(item_id: int = Query(..., description="Id of item to delete")):
+    if item_id not in inventory:
+        return {"Error": "Item ID dont exist"}
+    del inventory[item_id]
+    return {"msg": "Item has been deleted"}
+    
+     
     
 
 
